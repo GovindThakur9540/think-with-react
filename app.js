9d9da3f9f6259a52e15119f35182dceffae01cf5
Govind Thakur <77549669+GovindThakur9540@users.noreply.github.com>
2021-03-26 10:53:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+/*const Container = () =>{
+    return React.createElement(`div`,null,`Hey! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's learn react`)
+       );
+}
+// Render the Container
+const container = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);*/
+
+
+
 class ReactContainer extends React.Component{
     // JSX tags
     // JSX there should be one parent element
